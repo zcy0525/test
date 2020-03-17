d382bb54d29c27bc42b671bf1e445955776f5251
zcy0525 <15050840350@163.com>
2020-03-17 14:03:04 +0800

    Initial commit

---
+++ test-controller/src/main/java/com/example/testcontroller/TestControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestControllerApplication.class, args);
+    }
+
+}
---
+++ test-controller/src/test/java/com/example/testcontroller/TestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
